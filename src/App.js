39c96783b93f2d783cfb6dbb5f7689c463056235
YevaMV <eva280988@gmail.com>
2022-06-11 16:14:11 +0400

    added Links, Navigation and Style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import AllMeetups from './pages/AllMeetups';
 import CreateMeetup from './pages/CreateMeetup';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetups />} exact />
         <Route path="/new-meetup" element={<CreateMeetup />} />
