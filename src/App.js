c75b51dbe31a030390d1fa04521ec5c49286ca0f
YevaMV <eva280988@gmail.com>
2022-06-11 22:29:43 +0400

    added Lists of Data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import AllMeetups from './pages/AllMeetups';
 import CreateMeetup from './pages/CreateMeetup';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetups />} exact />
         <Route path="/new-meetup" element={<CreateMeetup />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
