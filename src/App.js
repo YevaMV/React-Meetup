45cbf2c70fd04235b413ffb22c2dc539798b2727
YevaMV <eva280988@gmail.com>
2022-06-11 15:30:30 +0400

    added Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import AllMeetups from './pages/AllMeetups';
+import CreateMeetup from './pages/CreateMeetup';
+
 function App() {
-  return <div>Meetup</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AllMeetups />} exact />
+        <Route path="/new-meetup" element={<CreateMeetup />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
