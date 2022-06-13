5257dca4fe6e23a41bdfbf52358c6481499f10fc
YevaMV <eva280988@gmail.com>
2022-06-13 19:54:49 +0400

    POST request

--- src/meetup/NewMeetupForm.js
+++ src/meetup/NewMeetupForm.js
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import CardForForm from '../ui/CardForForm';
 import classes from './NewMeetupForm.module.css';
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -22,7 +22,7 @@ function NewMeetupForm() {
       description: enteredDescription,
     };
 
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   }
 
   return (
--- src/pages/CreateMeetup.js
+++ src/pages/CreateMeetup.js
@@ -1,10 +1,22 @@
 import NewMeetupForm from '../meetup/NewMeetupForm';
 
 function CreateMeetup() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-meetup-d871b-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  }
   return (
     <section>
       <h1>Create Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
