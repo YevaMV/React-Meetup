ce365b3cf75d2c14a31c1e7c672512764c8e5b8e
YevaMV <eva280988@gmail.com>
2022-06-21 16:06:13 +0400

    added Context logic

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -8,6 +8,22 @@ const FavoritesContext = createContext({
 function FavoritesContextProvider(props) {
   const [userFavorites, setUserFavorites] = useState([]);
 
+  function addfavoritehandler(favoriteMeetup) {
+    setUserFavorites((prevuserFavorites) => {
+      return prevuserFavorites.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevuserFavorites) => {
+      return prevuserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
+
   const context = {
     favorites: userFavorites,
     totalFavorites: userFavorites.length,
