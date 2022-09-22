623ca2e8e8790430abfb911541c293fb26620d89
YevaMV <eva280988@gmail.com>
2022-09-22 17:34:53 +0400

    redirecting

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,11 @@
 import { useState, useRef, useContext, Fragment } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
-  const [formIsShown, setFormIsShown] = useState(true);
+  const navigate = useNavigate();
   const [isLogin, setIsLogin] = useState(true);
   const [error, setError] = useState(null);
   const emailInputRef = useRef();
@@ -42,7 +43,6 @@ const AuthForm = () => {
     })
       .then((res) => {
         if (res.ok) {
-          setFormIsShown(false);
           return res.json();
         } else {
           return res.json().then((data) => {
@@ -56,6 +56,7 @@ const AuthForm = () => {
       })
       .then((data) => {
         authCtx.login(data.idToken);
+        navigate('/');
       })
       .catch((err) => {
         setError(err.message);
@@ -64,38 +65,35 @@ const AuthForm = () => {
 
   return (
     <Fragment>
-      {!formIsShown && <h1>Welcome!!!</h1>}
-      {formIsShown && (
-        <section className={classes.auth}>
-          <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-          <form onSubmit={submitHandler}>
-            <div className={classes.control}>
-              <label htmlFor="email">Your Email</label>
-              <input type="email" id="email" required ref={emailInputRef} />
-            </div>
-            <div className={classes.control}>
-              <label htmlFor="password">Your Password</label>
-              <input
-                type="password"
-                id="password"
-                required
-                ref={passwordInputRef}
-              />
-            </div>
-            <div className={classes.actions}>
-              <button>{isLogin ? 'Login' : 'Create Account'}</button>
-              <button
-                type="button"
-                className={classes.toggle}
-                onClick={switchAuthModeHandler}
-              >
-                {isLogin ? 'Create new account' : 'Login with existing account'}
-              </button>
-            </div>
-          </form>
-          {error && <h1 className={classes['error-message']}>{error}</h1>}
-        </section>
-      )}
+      <section className={classes.auth}>
+        <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+        <form onSubmit={submitHandler}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input type="email" id="email" required ref={emailInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="password">Your Password</label>
+            <input
+              type="password"
+              id="password"
+              required
+              ref={passwordInputRef}
+            />
+          </div>
+          <div className={classes.actions}>
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+            <button
+              type="button"
+              className={classes.toggle}
+              onClick={switchAuthModeHandler}
+            >
+              {isLogin ? 'Create new account' : 'Login with existing account'}
+            </button>
+          </div>
+        </form>
+        {error && <h1 className={classes['error-message']}>{error}</h1>}
+      </section>
     </Fragment>
   );
 };
